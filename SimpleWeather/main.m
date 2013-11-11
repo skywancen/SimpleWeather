c9e483cdeea06631e46af8377d0fe762188479c5
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2013-11-11 18:38:18 -0500

    Initial Commit

---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Ryan Nystrom on 11/11/13.
+//  Copyright (c) 2013 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
